e5e9882da523a2e3a284e0f0a1bb0249d4ad3e5d
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-16 11:53:22 -0800

    added my planner provider to app and added useEntries to entrylist

--- src/components/Planner/EntryList.jsx
+++ src/components/Planner/EntryList.jsx
@@ -3,14 +3,15 @@ import styles from './EntryList.css';
 
 export default function Entries() {
   // TODO: Replace with the entries stored in PlannerContext
-  const entries = [
-    {
-      id: 0,
-      title: 'Replace me!',
-      content: '`content` should only be visible on the detail view',
-      date: new Date()
-    },
-  ];
+  const { entries } = useEntries();
+  // const entries = [
+  //   {
+  //     id: 0,
+  //     title: 'Replace me!',
+  //     content: '`content` should only be visible on the detail view',
+  //     date: new Date()
+  //   },
+  // ];
 
   return (
     <ul className={styles.list}>
